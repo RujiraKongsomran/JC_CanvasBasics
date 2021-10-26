f90f65b7a05afa66a5ae4fd575bac1d9c8810ab6
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-10-26 09:27:10 +0700

    Draw Circle

--- app/src/main/java/com/rujirakongsomran/jc_canvasbasics/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_canvasbasics/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,6 +50,14 @@ fun MyCanvas() {
             style = Stroke(
                 width = 3.dp.toPx()
             )
+        )
+        drawCircle(
+            brush = Brush.radialGradient(
+                colors = listOf(Color.Red, Color.Yellow),
+                center = center,
+                radius = 100f
+            ),
+            radius = 100f,
         )
     }
 }
