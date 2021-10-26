76cf306795e3fc30b8f530b1a9ce04e8a239f517
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-10-26 09:40:41 +0700

    Draw Oval

--- app/src/main/java/com/rujirakongsomran/jc_canvasbasics/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_canvasbasics/MainActivity.kt
@@ -70,6 +70,11 @@ fun MyCanvas() {
                 width = 3.dp.toPx()
             )
         )
+        drawOval(
+            color = Color.Magenta,
+            topLeft = Offset(500f, 100f),
+            size = Size(200f, 300f)
+        )
     }
 }
 
